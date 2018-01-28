c91fcd86a445f1ae62739fc38a5feb4fe644d3b8
minimallinux <anthonymcgowan1@gmail.com>
2018-01-28 17:02:59 +0000

    Update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # mobilemenu2
-Mobile Resposive Header Nav Menu using CSS and javascript (for mobile toggle)
+Mobile Resposive Header Nav Menu using CSS and javascript fade in/out (for mobile toggle)
 Straightforward and easy to style in dark or light colors.
 One media query to cover up to 900px sizes.
 Image or Text Logo/Brand on left currently set to image. (Uncomment h1 style and adjust padding in primary_nav ul settings if using text Brand.)
--- js/app.js
+++ js/app.js
@@ -1,14 +1,41 @@
 /*jslint browser:true */
-function showHide(event)
-{
-var div = document.querySelector("#primary_nav");
-if (div.style.display !== "none") {
-    div.style.display = "none";
-}
-else {
-    div.style.display = "block";
-}
-event.stopPropagation();
- }	
-document.querySelector('.js-btn').addEventListener('click', showHide); 
+// fade out
+function fadeOut(el){
+  el.style.opacity = 1;
 
+  (function fade() {
+    if ((el.style.opacity -= .1) < 0) {
+      el.style.display = 'none';
+      el.classList.add('is-hidden');
+    } else {
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+// fade in
+function fadeIn(el, display){
+  if (el.classList.contains('is-hidden')){
+    el.classList.remove('is-hidden');
+  }
+  el.style.opacity = 0;
+  el.style.display = display || "block";
+
+  (function fade() {
+    var val = parseFloat(el.style.opacity);
+    if (!((val += .1) > 1)) {
+      el.style.opacity = val;
+      requestAnimationFrame(fade);
+    }
+  })();
+}
+var btn = document.querySelector('.js-btn');
+var el = document.querySelector('#primary_nav');
+
+btn.addEventListener('click', function(e){
+  if(el.classList.contains('is-hidden')){
+    fadeIn(el);
+  }
+  else {
+    fadeOut(el);
+  }
+});
