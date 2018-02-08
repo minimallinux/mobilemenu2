b0b79741d152e693252a8d0b50de1970d587bccc
minimallinux <anthonymcgowan1@gmail.com>
2018-02-08 09:42:46 +0000

    Update To FA

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 /*jslint browser:true */
 // fade out
+var btn = document.querySelector('.js-btn');
+var el = document.querySelector('#primary_nav');
+var m = document.querySelector(".fa-bars");
+// fade out
 function fadeOut(el){
   el.style.opacity = 1;
-
   (function fade() {
     if ((el.style.opacity -= .1) < 0) {
       el.style.display = 'none';
       el.classList.add('is-hidden');
+     
     } else {
       requestAnimationFrame(fade);
     }
@@ -16,6 +20,7 @@ function fadeOut(el){
 function fadeIn(el, display){
   if (el.classList.contains('is-hidden')){
     el.classList.remove('is-hidden');
+
   }
   el.style.opacity = 0;
   el.style.display = display || "block";
@@ -28,16 +33,20 @@ function fadeIn(el, display){
     }
   })();
 }
-
-var btn = document.querySelector('.js-btn');
-var el = document.querySelector('#primary_nav');
-btn.addEventListener('click', function(e){
-  if(el.classList.contains('is-hidden')){
+function openCloseMenu() {
+ if(el.classList.contains('is-hidden')){
+    m.classList.remove('fa-bars');
+    m.classList.add('fa-times');
     fadeIn(el);
   }
   else {
+    m.classList.remove('fa-times');
+    m.classList.add('fa-bars');
     fadeOut(el);
   }
+}
+btn.addEventListener('click', function(){
+openCloseMenu();
 });
 
 
